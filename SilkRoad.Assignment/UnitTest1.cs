1f3b21227ffcd5e0d7836ab45cdf671e196bc24f
jaisondthomas <jaisondthomas@gmail.com>
2018-07-31 23:42:08 -0600

    Add project files.

---
+++ SilkRoad.Assignment/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SilkRoad.Assignment
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
